5216c418dd97b582fa5f38601dcdc091756c5a54
muxie2007 <nsubuga.muhsin@gmail.com>
2023-03-25 21:30:04 -0500

    Small change

---
+++ JavaScript/Personal folder/Todo app/script.js
@@ -0,0 +1,8 @@
+let itemArray = [];
+let taskInput = document.querySelector('#input-task input')
+let addButton = document.querySelector('#add-task')
+let addToDoContainer = document.getElementById("todosContainer");
+addButton.addEventListener("click", addToDo);
+function addToDo() {
+
+}
