3423a01178ebcf82e2cc3aa6ab230714a92354d4
muxie2007 <nsubuga.muhsin@gmail.com>
2023-03-31 20:50:12 -0500

    simple ipdate

--- JavaScript/Personal folder/Todo app/script.js
+++ JavaScript/Personal folder/Todo app/script.js
@@ -3,6 +3,11 @@ let taskInput = document.querySelector('#input-task input')
 let addButton = document.querySelector('#add-task')
 let addToDoContainer = document.getElementById("todosContainer");
 addButton.addEventListener("click", addToDo);
-function addToDo() {
+// function addToDo() {
+//     let newTodoText = addToDoContainer.value;
+//        for (let index = 0; index < itemArray.length; index++) {
+//       const todoObject = itemArray[index];
+//          }
 
-}
+//          console.log(itemArray)
+// }
