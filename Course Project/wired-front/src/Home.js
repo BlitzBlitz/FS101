a0e1591d375d33e13fe86cd8d0708e3e152423da
WikiRon <ron.kyamb@gmail.com>
2023-04-28 21:47:06 -0500

    first branch update

--- Course Project/wired-front/src/Home.js
+++ Course Project/wired-front/src/Home.js
@@ -1,11 +1,16 @@
-import Hero from "./Hero";
-import "./Home.css";
-import SidePostContainer from "./SidePostContainer";
+import Hero from './Hero';
+import './Home.css';
+import SidePostContainer from './SidePostContainer';
+import Trending from './Trending';
+
 export default function Home() {
   return (
-    <div className="home">
-      <Hero></Hero>
-      <SidePostContainer></SidePostContainer>
-    </div>
+    <>
+      <div className="home">
+        <Hero></Hero>
+        <SidePostContainer></SidePostContainer>
+      </div>
+      <Trending></Trending>
+    </>
   );
 }
